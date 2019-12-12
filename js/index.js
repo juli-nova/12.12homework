f6925d1d6556bf0ec7e7865f382889e84b3810fc
juli-nova <juli.novaa@gmail.com>
2019-12-12 21:27:02 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+const sect = document.querySelector('section');
+const para = document.createElement('p');
+para.classList.add('dynamic');
+
+
+document.querySelector('.calc').addEventListener('click', );
+function calc() {
+    const display = document.querySelector('.display');
+    display.value = eval(display.value);
+}
+document.querySelector('.clear').addEventListener('delete', clear);
+function delete() {
