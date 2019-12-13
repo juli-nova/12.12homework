784f6a6cadc7766469808ea582a2057e85724d4c
juli-nova <juli.novaa@gmail.com>
2019-12-13 14:34:48 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
-
-const sect = document.querySelector('section');
-const para = document.createElement('p');
-para.classList.add('dynamic');
-
-
-document.querySelector('.calc').addEventListener('click', );
-function calc() {
-    const display = document.querySelector('.display');
-    display.value = eval(display.value);
-}
-document.querySelector('.clear').addEventListener('delete', clear);
-function delete() {
+    <script>
+      const list = document.querySelector('ul');
+      const input = document.querySelector('input');
+      const button = document.querySelector('button');
+      button.onclick = function() {
+        let myItem = input.value;
+        input.value = '';
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = 'Delete';
+        list.appendChild(listItem);
+        listBtn.onclick = function(e) {
+          list.removeChild(listItem);
+        }
+        input.focus();
+      }
